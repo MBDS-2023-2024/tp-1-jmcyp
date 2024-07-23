5338055ff67817af129b44f843a47b1d495e2f28
Jean Maxo Cyprien <jmcyprien@solutions.ht>
2024-07-23 15:59:45 -0400

    Test affichage de la liste des 10 etudiants

--- app/src/main/java/com/gmail/eamosse/tp2/Student.kt
+++ app/src/main/java/com/gmail/eamosse/tp2/Student.kt
@@ -1,11 +1,11 @@
 package com.gmail.eamosse.tp2
 
 //TODO : Complete the class
-data class Student {
-    val name: String,
-    val code: String,
-    val sexe: String,
-    val address: String,
-    val age: Int
+data class Student (
+    var name: String,
+    var code: String,
+    var sexe: String,
+    var address: String,
+    var age: Int
 
-}
+)
