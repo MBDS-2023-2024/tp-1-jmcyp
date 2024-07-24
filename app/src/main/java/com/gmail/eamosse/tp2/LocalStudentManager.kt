121824eecfc97c8540f5cd9481a6be3240ff81ca
Jean Maxo Cyprien <jmcyprien@solutions.ht>
2024-07-23 22:44:22 -0400

    Creation de la liste contenant uniquement les filles ou les garcons

--- app/src/main/java/com/gmail/eamosse/tp2/LocalStudentManager.kt
+++ app/src/main/java/com/gmail/eamosse/tp2/LocalStudentManager.kt
@@ -6,7 +6,7 @@ class LocalStudentManager : StudentManager {
     // TODO 2: init the class with a list of 10 students every time a new instance is created;
     // Use the followi
 
-    val etudiants = listOf(
+    val etudiants = mutableListOf(
         Student(name = "John1", code = "A1", sexe = "M", address = "Lilles", age = 20),
         Student(name = "John2", code = "A2", sexe = "F", address = "Lilles", age = 30),
         Student(name = "John3", code = "A3", sexe = "F", address = "Lilles", age = 41),
@@ -37,7 +37,11 @@ class LocalStudentManager : StudentManager {
     }
 
     override fun boysOrGirls(sexe: String): List<Student> {
-        TODO("Not yet implemented")
+        return when (sexe) {
+            "F" -> etudiants.filter { it.sexe == "F" }
+            "M" -> etudiants.filter { it.sexe == "M" }
+            else -> emptyList()
+        }
     }
 
     override fun onlyNames(): List<String> {
